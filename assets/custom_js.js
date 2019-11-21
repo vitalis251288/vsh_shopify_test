67b15edd74a3eeace9bad56fdb8c403d7ae724cf
vitalis251288 <vitalis251288@gmail.com>
2019-11-21 22:30:32 +0200

    end

--- assets/custom_js.js
+++ assets/custom_js.js
@@ -3,12 +3,17 @@
 
 var NS = {
 	test_func: function(){
-		$("input[type=submit].btn").on("click", function(){
+		var t = false;
+		$(".btn[type=submit]").on("click", function(){
 			if($("#CartCount span[data-cart-count]").text() !== "0"){
 				$("#CartCount").removeClass("hide");
 			}else{
 				$("#CartCount").addClass("hide");
+			}	
+			if(t == false){
+				$("#CartCount").removeClass("hide");
 			}
+			t = true;
 		});
 	},
 	init: function(){
@@ -18,4 +23,12 @@ var NS = {
 
 $(window).on("load", function(){
 	NS.init();
+
+	/*$("img.vsh_nearly_swatch").on("click", function(e){
+		var $src = $(this).attr("src");
+		var $srcset = $(this).attr("srcset");
+		$(this).closest("li.grid__item").find("img.grid-view-item__image").attr("src", $src);
+		$(this).closest("li.grid__item").find("img.grid-view-item__image").attr("srcset", $srcset);
+	});*/
+
 });
--- assets/theme.js
+++ assets/theme.js
@@ -5134,3 +5134,28 @@ $(document).ready(function() {
     }
   }
 });
+
+
+
+$(document).ready(function() {
+  thumbnails = $('img[src*="/products/"]').not(':first');
+  if (thumbnails.length) {
+    thumbnails.bind('click', function() {
+      var arrImage = $(this).attr('src').split('?')[0].split('.');
+      var strExtention = arrImage.pop();
+      var strRemaining = arrImage.pop().replace(/_[a-zA-Z0-9@]+$/,'');
+      var strNewImage = arrImage.join('.')+"."+strRemaining+"."+strExtention;
+      if (typeof variantImages[strNewImage] !== 'undefined') {
+          productOptions.forEach(function (value, i) {
+           optionValue = variantImages[strNewImage]['option-'+i];
+           if (optionValue !== null && $('.single-option-selector:eq('+i+') option').filter(function() { return $(this).text() === optionValue }).length) {
+             $('.single-option-selector:eq('+i+')').val(optionValue).trigger('change');
+           }
+        });
+      }
+    });
+  }
+});
+
+
+
